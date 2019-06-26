fef33b0e67ccaeded4d0cf74f9a6798e224bb035
Jørgen Kvalsvik <jokva@equinor.com>
2019-06-26 18:24:58 +0200

    Add summary-specification
    The summary specification is incomplete, in the sense that it's not
    really useful yet. It does, however, set up some infrastructure (both CI
    and module layout), and demonstrates more-or-less the design direction
    of the summary and summary specification.

--- python/setup.py
+++ python/setup.py
@@ -4,6 +4,15 @@ import os
 import skbuild
 import setuptools
 
+class get_pybind_include(object):
+    def __init__(self, user=False):
+        self.user = user
+
+    def __str__(self):
+        # postpone importing pybind11 until building actually happens
+        import pybind11
+        return pybind11.get_include(self.user)
+
 def src(x):
     root = os.path.dirname( __file__ )
     return os.path.abspath(os.path.join(root, x))
@@ -33,6 +42,11 @@ def getversion():
 
     return pkgversion
 
+pybind_includes = [
+    str(get_pybind_include()),
+    str(get_pybind_include(user = True))
+]
+
 skbuild.setup(
     name = 'ecl3',
     description = 'ecl3',
@@ -40,6 +54,7 @@ skbuild.setup(
     url = 'https://github.com/equinor/ecl3',
     packages = [
         'ecl3',
+        'ecl3.summary',
     ],
     license = 'LGPL-3.0',
     platforms = 'any',
@@ -53,8 +68,14 @@ skbuild.setup(
     tests_require = [
         'pytest',
         'hypothesis',
+        'path.py',
     ],
+    # we're building with the pybind11 fetched from pip. Since we don't rely on
+    # a cmake-installed pybind there's also no find_package(pybind11) -
+    # instead, the get include dirs from the package and give directly from
+    # here
     cmake_args = [
+        '-DPYBIND11_INCLUDE_DIRS=' + ';'.join(pybind_includes),
         # we can safely pass OSX_DEPLOYMENT_TARGET as it's ignored on
         # everything not OS X. We depend on C++11, which makes our minimum
         # supported OS X release 10.9
